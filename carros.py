2f3881c7ee79fec6b245569e525d60e00062d6d0
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2023-10-02 15:16:37 +0100

    ola

--- carros.py
+++ carros.py
@@ -1,5 +1,22 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 data = pd.read_excel('CarDataset.xlsx')
+varNames = data.columns.values.tolist()
 print(data)
-#g3gwrgq35g
+dataMatrix = data.to_numpy()
+
+#MPG é o index 6 da lista
+MPG = dataMatrix[:,6]
+
+for i in range (6):
+    plt.figure(2)
+    plt.subplot(321+i)
+    plt.scatter(dataMatrix[:,i], MPG, color = 'purple')
+    plt.xlabel(varNames[i])
+    plt.ylabel("MPG")
+    plt.title("MPG vs. " + varNames[i])
+    
+plt.show()
+
+print(MPG)
