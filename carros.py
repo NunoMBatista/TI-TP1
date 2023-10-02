c7250dea02cca5734cf6dbef0892f4f6349341d6
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2023-10-02 15:39:36 +0100

    a

--- carros.py
+++ carros.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pd.read_excel('CarDataset.xlsx')
 varNames = data.columns.values.tolist()
-print(data)
 dataMatrix = data.to_numpy()
 #sdfs
 
@@ -12,13 +12,13 @@ dataMatrix = data.to_numpy()
 MPG = dataMatrix[:,6]
 
 for i in range (6):
-    plt.figure(2)
     plt.subplot(321+i)
     plt.scatter(dataMatrix[:,i], MPG, color = 'purple')
     plt.xlabel(varNames[i])
     plt.ylabel("MPG")
     plt.title("MPG vs. " + varNames[i])
     
+plt.tight_layout()
 plt.show()
 
-print(MPG)
+print(np.max(dataMatrix))
