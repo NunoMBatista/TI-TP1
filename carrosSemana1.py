e4cba05b3f59b03abd398a84579540704970931f
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2023-10-02 20:10:46 +0100

    Final da semana1 - Início (e fim) da semana 2

--- carrosSemana1.py
+++ carrosSemana1.py
@@ -2,15 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def ocorrencias (varIndex):
+    contador = alfa
+    for i in dataMatrix[:,varIndex]:
+        contador[i] += 1
+    return contador    
+
 data = pd.read_excel('CarDataset.xlsx')
 varNames = data.columns.values.tolist()
 dataMatrix = data.to_numpy()
-
 
 #MPG é o index 6 da lista
 MPG = dataMatrix[:,6]
 
 for i in range (6):
+    plt.figure(1)
     plt.subplot(321+i)
     plt.scatter(dataMatrix[:,i], MPG, color = 'purple')
     plt.xlabel(varNames[i])
@@ -23,8 +29,4 @@ plt.show()
 dataMatrix = dataMatrix.astype("uint16") 
 alfa = {key: 0 for key in range (np.min(dataMatrix), np.max(dataMatrix) + 1)}
 
-dataArray = np.reshape(dataMatrix, -1)
-for i in dataArray:
-    alfa[i] += 1
-    
-print(alfa)
+#Falta entender o que dizer quanto à relação de MPG com as restantes variáveis
---
+++ carrosSemana2.py
@@ -0,0 +1,73 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
+
+def compareMPG():
+    #MPG é o index 6 da lista
+    MPG = dataMatrix[:,6]
+
+    for i in range (6):
+        plt.figure(1)
+        plt.subplot(321+i)
+        plt.scatter(dataMatrix[:,i], MPG, color = 'purple')
+        plt.xlabel(varNames[i])
+        plt.ylabel("MPG")
+        plt.title("MPG vs. " + varNames[i])
+    plt.tight_layout()
+
+def ocorrencias (varIndex):
+    contador = alfa
+    for i in dataMatrix[:,varIndex]:
+        contador[i] += 1
+    return contador    
+
+def ocorrenciasPlot (varIndex):
+    contador = ocorrencias(varIndex)
+    xAxis = [x for x in contador.keys() if contador[x] > 0]
+    yAxis = []
+    for i in xAxis:
+        yAxis.append(contador[i])
+        
+    # O x_values é uma linha de valores para o eixo X sem espaços vazios
+    x_values = np.arange(len(xAxis))
+
+    plt.figure(2)
+    plt.bar(x_values, yAxis, color = "red")
+    plt.xlabel(varNames[varIndex])
+    plt.ylabel("Count")
+    # xticks é usado para trocar as labels do x_values pelas do xAxis, tendo assim 
+    # uma linha não interrompida de valores em xmas com as labels corretas do xAxis    
+    plt.xticks(x_values, xAxis)
+    plt.axis("tight")
+    plt.tight_layout()
+
+def binning (varIndex, n):
+    target = dataMatrix[:,varIndex]
+    binningN = len(target) // n
+    #Dividir weight em binningN subarrays
+    binnings = [target[i * n : (i + 1) * n] for i in range(binningN)]
+
+    for binning in binnings: 
+        count = np.bincount(binning)
+        replacement = np.argmax(count)
+        binning[True] = replacement
+
+    target = np.reshape(target, -1)
+    return target
+
+data = pd.read_excel('CarDataset.xlsx')
+varNames = data.columns.values.tolist()
+dataMatrix = data.to_numpy()
+
+dataMatrix = dataMatrix.astype("uint16") 
+alfa = {key: 0 for key in range (np.min(dataMatrix), np.max(dataMatrix) + 1)}
+
+ocorrenciasPlot(0)
+compareMPG()
+  
+weight = binning(6, 40)
+displacement = binning(2, 5)
+horsepower = binning(3, 5)
+
+plt.show()
+#Falta entender o que dizer quanto à relação de MPG com as restantes variáveis
