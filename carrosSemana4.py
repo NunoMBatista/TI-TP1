e332d14cb1ce8ed728449fbcf243288c4959d8db
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2023-10-27 21:17:48 +0100

    adf

--- carrosSemana4.py
+++ carrosSemana4.py
@@ -34,15 +34,26 @@ def entropyHuff (target, alfa):
     codec = huffc.HuffmanCodec.from_data(target) 
     symbols, lengths = codec.get_code_len()
 
-    variancia = np.var(lengths, ddof = 0)    
-    print("Variância de Comprimentos:", variancia)
-
     ocorr = ocorrencias(target, alfa)   
     tamanho = len(target)
 
     probabilidades = [(ocorr[symbols[x]]/tamanho)*lengths[x] for x in range(len(symbols))]
-    return sum(probabilidades)
- 
+    entropy = sum(probabilidades)
+    
+    variancia = var(target, symbols, lengths, entropy, ocorr)   
+    print("Variância de Comprimentos:", variancia)
+    
+    return entropy
+
+def var(target, symbols, lengths, entropy, ocorr):
+    tamanho = len(target)
+    variance = 0
+    for idx in range(len(symbols)):
+        prob = ocorr[symbols[idx]] / tamanho
+        # Calcula a diferença quadrática ponderada entre o comprimento e a entropia
+        variance += prob * (lengths[idx] - entropy) ** 2
+    return variance
+
 def entropy(target, alfa):
     # H(X) = -ΣP(i)*log2(P(i))
     contador = ocorrencias(target, alfa)
@@ -50,7 +61,7 @@ def entropy(target, alfa):
     maior = max(target)
     tamanho = len(target)
     
-    probabilidades = [(contador[x]/tamanho)*math.log2(contador[x]/tamanho) for x in range(menor, maior) if contador[x] > 0]
+    probabilidades = [(contador[x]/tamanho)*math.log2(contador[x]/tamanho) for x in range(menor, maior+1) if (contador[x] > 0)]
     
     return -sum(probabilidades)  
     
@@ -84,12 +95,14 @@ def ocorrenciasPlot (target, alfa, name, tickInterval, figura):
     yAxis = []
     for i in xAxis:
         yAxis.append(contador[i]) 
+        
     # O x_values é uma linha de valores para o eixo X sem espaços vazios
     x_values = np.arange(len(xAxis))
     plt.figure(figura)
     plt.bar(x_values, yAxis, color = "red")
     plt.xlabel(name)
     plt.ylabel("Count")
+    
     # xticks é usado para   trocar as labels do x_values pelas do xAxis, tendo assim 
     # uma linha não interrompida de valores em xmas com as labels corretas do xAxis    
     
@@ -102,11 +115,11 @@ def ocorrenciasPlot (target, alfa, name, tickInterval, figura):
     
 def binning (target, n, firstAlfa):
     lastAlfa = np.max(target)
-    targetAlfa = {key: 0 for key in range(firstAlfa, lastAlfa + 1)}
+    targetAlfa = {key: 0 for key in range(0, lastAlfa + 1)}
     ocorr = ocorrencias(target, targetAlfa)
     binningN = len(list(ocorr.keys())) // n
     for i in range(binningN):
-        binn = list(ocorr.keys())[i * n : (i+1) * n]
+        binn = list(ocorr.keys())[i * n : ((i+1) * n)+1]
         replacement = max(ocorr, key = lambda k: ocorr[k] if k in binn else -1)
         mask = (target >= np.min(binn)) & (target <= np.max(binn))
         target[mask] = replacement
@@ -119,7 +132,7 @@ dataMatrix = data.to_numpy()
 dataMatrix = dataMatrix.astype("uint16")
 
 # Definir alfabeto
-alfa = {key: 0 for key in range (np.min(dataMatrix), np.max(dataMatrix) + 1)}
+alfa = {key: 0 for key in range(0, 65535)} #todos os Uint16
 
 acceleration = dataMatrix[:,0]
 cylinders = dataMatrix[:,1]
@@ -131,12 +144,12 @@ weight = dataMatrix[:,5]
 compareMPG(dataMatrix, varNames)
 
 ocorrenciasPlot(acceleration, alfa, "Acceleration", 1, 2)
-weight = binning(dataMatrix[:,5], 200, np.min(dataMatrix[:,5]))
-ocorrenciasPlot(weight, alfa, "Weight", 1, 3)
+weight = binning(dataMatrix[:,5], 40, np.min(dataMatrix[:,5]))
+ocorrenciasPlot(weight, alfa, "Weight", 7, 3)
 distance = binning(dataMatrix[:,2], 5, np.min(dataMatrix[:,2]))
 ocorrenciasPlot(distance, alfa, "Distance", 5, 4)
 horsepower = binning(dataMatrix[:,3], 5, np.min(dataMatrix[:,3]))
-ocorrenciasPlot(horsepower, alfa, "Horse Power", 1, 5)
+ocorrenciasPlot(horsepower, alfa, "Horse Power", 3, 5)
 
 MPG = dataMatrix[:,6]
 IMarray = []
@@ -160,6 +173,8 @@ print(MPGpred)
 MPGpred = -5.5241 - 0.146 * IMarray[0] - 0.4909 * IMarray[1] + 0.0026 * IMarray[2] - 0.0045 * IMarray[3] + 0.6725 * IMarray[4] - 0.0059 * 0
 print(MPGpred)
 
-#plt.show()
+#print(entropy (np.reshape(dataMatrix, -1)))
+
+plt.show()
 
 #CALCULAR VARIÂNCIA NO EXERCÍCIO 8
