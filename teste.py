649d3a76811b58b244f3be7c65238414859f2ddd
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2023-10-30 16:22:28 +0000

    sdg

--- teste.py
+++ teste.py
@@ -2,5 +2,6 @@ import numpy as np
 
 X = [1, 2, 2, 4, 2, 6, 5, 5, 10, 10, 5]
 Y = [1, 2, 3, 4, 2, 5, 3, 3, 10, 11, 3]
-
-print(np.sum((X = 5) & (Y = 3)))
+X = np.array(X)
+Y = np.array(Y)
+print(np.sum((X == 5) & (Y == 3)))
